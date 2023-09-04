db04a036b7a0e467c8af095c017d2ad1da2e40db
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-04 15:31:15 -0500

    working on getting it to work

---
+++ src/components/Nav.jsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+    return (
+        <navbar id='Nav'>
+            <a id="aboutme" href="./src/components/AboutMe.jsx">About Me</a>
+            <a id="portfolio" href="./src/components/Portfolio.jsx">Portfolio</a>
+            <a id='contact' href="#ContactMe">Contact</a>
+            <a id="resume" href="Resume.img">Resume</a>
+        </navbar>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AboutMe from './components'
+import AboutMe from './components/AboutMe'
 import Portfolio from './components/Portfolio.jsx'
 import './index.css'
 

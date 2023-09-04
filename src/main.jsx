31bac122b0a556408533afd3367453955fcdda07
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-04 15:33:38 -0500

    still working on getting it to work

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import Nav from './components/Nav.jsx'
 import AboutMe from './components/AboutMe'
 import Portfolio from './components/Portfolio.jsx'
 import './index.css'

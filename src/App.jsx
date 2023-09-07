cb039ccbe8028d1df90cac98e26723cb8f92694c
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-07 13:40:31 -0500

    working on getting the nav to work at the top

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,18 @@ import Nav from './components/NavTabs';
 import Portfolio from './pages/Portfolio';
 import { Outlet } from 'react-router-dom';
 
-
+import PageHeader from 'react-router-dom'
 
 
 export default function App() {
   return (
     <Routes>
+         {/* <PageHeader path="Nav" element={<Nav />} /> */}
           <Route
-            path="/" 
+            path="*" 
             element={ 
             <>
-            <Nav/>
+            <AboutMe/>
         <main className="mx-3">
           <Outlet />
         </main>

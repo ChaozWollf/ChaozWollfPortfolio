328ee37f146ecd92f06e85f54e3c0f3156a49f51
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-07 15:13:21 -0500

    working on getting the footer fixed

--- src/components/Footers.jsx
+++ src/components/Footers.jsx
@@ -1,11 +1,11 @@
 function Footer() {
     return (
-        < div id="footer">
-        // <a id= 'github' href='https://github.com/ChaozWollf'><img id='../../../images/GitHub-Mark.png'alt='a github mark logo'></img></a>
-         // <a id= 'linkedin' href='https://www.linkedin.com/in/brandon-witkowski-199765270/'><img id='../../../public/images/LinkedIn_logo_initials.png'alt='a linkdin logo'></img></a>
-         // <a id= 'meta' href='https://www.facebook.com/profile.php?id=100090098117999'><img id='../../../public/images/metaimages.png'alt='a Facebook logo'>'</img></a>
-        </div>
-        
+        // < div id="footerss">
+        // // <a id= 'github' href='https://github.com/ChaozWollf'><img id='../../../images/GitHub-Mark.png'alt='a github mark logo'></img></a>
+        //  // <a id= 'linkedin' href='https://www.linkedin.com/in/brandon-witkowski-199765270/'><img id='../../../public/images/LinkedIn_logo_initials.png'alt='a linkdin logo'></img></a>
+        //  // <a id= 'meta' href='https://www.facebook.com/profile.php?id=100090098117999'><img id='../../../public/images/metaimages.png'alt='a Facebook logo'>'</img></a>
+        // </div>
+       <h2>working on it</h2> 
 
 
     )

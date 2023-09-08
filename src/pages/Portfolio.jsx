6dbb132c06d87ff812a4ab070647c9b611ec3663
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-08 16:13:25 -0500

    functional but not pretty

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,16 +3,66 @@ function Portfolio() {
 <div>
             
             <div className="Weath-Dash">
-                <div className=""> </div>
-                <div className="card-body"><img className = 'Weather' href='../../../public/images/Screenshot 2023-09-08 134239.png'>
+                <div className="card-body"><img className = 'Weather' src='../../../public/images/Screenshot 2023-09-08 141544.png'>
                 </img>
                     
-                    <a className='ET port' href='https://github.com/ChaozWollf/Weath-Dash'  >Weather Dashboard Portfolio</a>
+                    <a className='WD_port' href='https://github.com/ChaozWollf/Weath-Dash'  >Weather Dashboard Portfolio</a>
                     
-                    <a className='ET App' href= 'https://chaozwollf.github.io/Weath-Dash/'  >Weather Dashboard App</a>
+                    <a className='WD_App' href= 'https://chaozwollf.github.io/Weath-Dash/'  >Weather Dashboard App</a>
                 </div>
             </div> 
-            
+            <div className="PasswordGen">
+                <div className="card-body"><img className='Password' src='../../../public/images/Screenshot 2023-09-08 142427.png'>
+                </img>
+
+                    <a className='PW_port' href='https://github.com/ChaozWollf/Password-Gen-code'  >Password Generator Portfolio</a>
+
+                    <a className='PW_App' href='https://chaozwollf.github.io/Password-Gen-code/'  >Password Generator App</a>
+                </div>
+            </div> 
+            <div className="WorkDay">
+                <div className="card-body"><img className='Work' src='../../../public/images/Screenshot 2023-09-08 143102.png'>
+                </img>
+
+                    <a className='WDS_port' href='https://github.com/ChaozWollf/Work-Day-Sched'  >Work Day Scheduler Portfolio</a>
+
+                    <a className='WDS_App' href='https://chaozwollf.github.io/Work-Day-Sched'  > Work Day Scheduler App</a>
+                </div>
+            </div> 
+            <div className="QuizQuery">
+                <div className=""> </div>
+                <div className="card-body"><img className='Quiz' src='../../../public/images/Screenshot 2023-09-08 144041.png'>
+                </img>
+
+                    <a className='QQ_port' href='https://github.com/ChaozWollf/Quiz-Query'  >Soon to be Deleted 
+                    Quiz Portfolio</a>
+
+                    <a className='QQ_App' href='https://chaozwollf.github.io/Quiz-Query/'  >Soon to be Deleted 
+                    Quiz App</a>
+                </div>
+            </div> 
+            <div className="QuoteOfDay">
+                <div className=""> </div>
+                <div className="card-body"><img className='Quote' src='../../../public/images/Screenshot 2023-09-08 144739.png'>
+                </img>
+
+                    <a className='QD_port' href='https://github.com/ChaozWollf/project-1'  >Quote of the Day Portfolio</a>
+
+                    <a className='QD_App' href='https://chaozwollf.github.io/project-1/'  > Quote of the Day App</a>
+                </div>
+            </div> 
+            <div className="Event_Lister">
+                <div className=""> </div>
+                <div className="card-body"><img className='event' src='../../../public/images/Screenshot 2023-09-08 152758.png'>
+                </img>
+
+                    <a className='EL_port' href='https://github.com/ChaozWollf/event-lister'  >Event Lister
+                     Portfolio</a>
+
+                    <a className='ET_App' href='https://event-lister2-65ba9b9031a9.herokuapp.com/schedule'  > Event Lister App</a>
+                </div>
+            </div> 
+
             
 </div>
     )

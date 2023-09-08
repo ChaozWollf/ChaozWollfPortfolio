581218031ea929234c2e64aad7f638f0e20b781b
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-08 13:53:24 -0500

    working on getting portfolio page up

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,23 @@
 function Portfolio() {
     return (
-<h2>This is the Portfolio section of the APP......this is just a test!!!</h2>)}
+<div>
+            
+            <div className="Weath-Dash">
+                <div className=""> </div>
+                <div className="card-body"><img className = 'Weather' href='../../../public/images/Screenshot 2023-09-08 134239.png'>
+                </img>
+                    
+                    <a className='ET port' href='https://github.com/ChaozWollf/Weath-Dash'  >Weather Dashboard Portfolio</a>
+                    
+                    <a className='ET App' href= 'https://chaozwollf.github.io/Weath-Dash/'  >Weather Dashboard App</a>
+                </div>
+            </div> 
+            
+            
+</div>
+    )
+}            
 
-
-
+    
+    
 export default Portfolio

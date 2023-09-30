b831badc3738a0c4d0100e5b0fa1ff4fe7a29a19
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-09-30 18:13:23 -0500

    working on Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -3,7 +3,7 @@ function Portfolio() {
 <div>
             
             <div className="Weath-Dash">
-                <div className="card-body"><img className = 'Weather' src='../../../public/images/Screenshot 2023-09-08 141544.png'>
+                <div className="card-body"><img className = 'Weather' src='../../../images/Screenshot 2023-09-08 141544.png'>
                 </img>
                     
                     <a className='WD_port' href='https://github.com/ChaozWollf/Weath-Dash'  >Weather Dashboard Portfolio</a>
@@ -12,7 +12,7 @@ function Portfolio() {
                 </div>
             </div> 
             <div className="PasswordGen">
-                <div className="card-body"><img className='Password' src='../../../public/images/Screenshot 2023-09-08 142427.png'>
+                <div className="card-body"><img className='Password' src='../../../images/Screenshot 2023-09-08 142427.png'>
                 </img>
 
                     <a className='PW_port' href='https://github.com/ChaozWollf/Password-Gen-code'  >Password Generator Portfolio</a>
@@ -21,7 +21,7 @@ function Portfolio() {
                 </div>
             </div> 
             <div className="WorkDay">
-                <div className="card-body"><img className='Work' src='../../../public/images/Screenshot 2023-09-08 143102.png'>
+                <div className="card-body"><img className='Work' src='../../../images/Screenshot 2023-09-08 143102.png'>
                 </img>
 
                     <a className='WDS_port' href='https://github.com/ChaozWollf/Work-Day-Sched'  >Work Day Scheduler Portfolio</a>
@@ -31,7 +31,7 @@ function Portfolio() {
             </div> 
             <div className="Queries">
                 <div className=""> </div>
-                <div className="card-body"><img className='Quieries' src='../../../public/images/Screenshot 2023-09-21 124342.png'>
+                <div className="card-body"><img className='Quieries' src='../../../images/Screenshot 2023-09-21 124342.png'>
                 </img>
 
                     <a className='CQ_port' href='https://github.com/ChaozWollf/conspiracyQueries'  >Conspieracy Queries Portfolio</a>
@@ -41,7 +41,7 @@ function Portfolio() {
             </div> 
             <div className="QuoteOfDay">
                 <div className=""> </div>
-                <div className="card-body"><img className='Quote' src='../../../public/images/Screenshot 2023-09-08 144739.png'>
+                <div className="card-body"><img className='Quote' src='../../../images/Screenshot 2023-09-08 144739.png'>
                 </img>
 
                     <a className='QD_port' href='https://github.com/ChaozWollf/project-1'  >Quote of the Day Portfolio</a>
@@ -51,7 +51,7 @@ function Portfolio() {
             </div> 
             <div className="Event_Lister">
                 <div className=""> </div>
-                <div className="card-body"><img className='event' src='../../../public/images/Screenshot 2023-09-08 152758.png'>
+                <div className="card-body"><img className='event' src='../../../images/Screenshot 2023-09-08 152758.png'>
                 </img>
 
                     <a className='EL_port' href='https://github.com/ChaozWollf/event-lister'  >Event Lister
